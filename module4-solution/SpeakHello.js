92cdc5e217771777c12a2fffaea4a62bb6dbe191
theprateekgoyal <prateek29081999@gmail.com>
2020-06-01 10:45:39 +0530

    My final Assignment 4 solution

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -18,7 +18,7 @@ helloSpeaker.speak=function(name) {
 }
 window.helloSpeaker=helloSpeaker;
 }) (window);
-helloSpeaker.speak(name);
+//helloSpeaker.speak(name);
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
--- module4-solution/script.js
+++ module4-solution/script.js
@@ -31,7 +31,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
+(function (window) {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -40,7 +40,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (var i = 0; i < names.length; i++) {
+for (var i =0 ; i< names.length; i++) {
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
@@ -50,7 +50,8 @@ for (var i = 0; i < names.length; i++) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt(0).toLowerCase();
-
+window.byeSpeaker.speak=byeSpeaker.speak;
+window.helloSpeaker.speak= helloSpeaker.speak;
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
@@ -63,4 +64,4 @@ for (var i = 0; i < names.length; i++) {
   }
 }
 
-})();
+})(window);
